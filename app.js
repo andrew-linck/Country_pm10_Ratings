b8bb241f77eacdd56f14b3fb1b52a6f22c8d51bb
Nihit Tiwari <nihitter05@gmail.com>
2019-01-29 13:49:05 -0500

    user input added

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-getCountryCode('mexico');
+const getCountryInput = function (event) {
+    event.preventDefault();
+    const countryName = $("#userInput").val().trim();
+    getCountryCode(countryName);
+}
+
+$("#searchButton").on("click", getCountryInput);
+
+// getCountryCode(countryName);
